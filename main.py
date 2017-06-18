69e1fedfc558172bc34ab71af828c5ba68ad6cf9
juzQrios <darshanjayadev@gmail.com>
2017-06-18 11:23:54 +0530

    A little touches

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ def randgenerator(board):
 
 rand_row = randgenerator(board)
 rand_col = randgenerator(board)
-
+win = 0
 for turn in range(5):
 	guess_row = int(input("Guess Row:"))
 	guess_col = int(input("Guess Column:"))
@@ -21,6 +21,8 @@ for turn in range(5):
 		print("CONGRATS! You just destroyed my Battleship") 
 		board[rand_row][rand_col] = "X"
 		print_board(board)
+		break
+		win = 1
 	elif guess_row not in range(5) or guess_col not in range(5):
 		print("You are far to away from my battleship!")
 	elif board[guess_row][guess_col] == "X":
@@ -31,12 +33,15 @@ for turn in range(5):
 		print("That's not my Battleship")
 		print_board(board)
 
-print("You failed to guess where my Battleship is! You freakin loose Sucker!")
-print("This was where my BattleShip was:")
-for x in range(5):
-	for i in range(5):
-		board[x][i] = "O"	
-board[rand_row][rand_col] = "X"
-print_board(board)
+if win = 1:
+	print("YOU WIN!")
+else:
+	print("You failed to guess where my Battleship is! You freakin loose Sucker!")
+	print("This was where my BattleShip was:")
+	for x in range(5):
+		for i in range(5):
+			board[x][i] = "O"	
+	board[rand_row][rand_col] = "X"
+	print_board(board)
 
 
